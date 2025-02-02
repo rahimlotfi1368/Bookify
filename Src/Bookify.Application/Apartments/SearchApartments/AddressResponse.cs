36bd66593ab9f1c6b1dfaf0f8c7768cc2c9d56e7
Rahim Lotfi Nemati <ra-lotfi@agri-bank.com>
2025-02-02 16:59:57 +0330

    Finish Apartments

---
+++ Src/Bookify.Application/Apartments/SearchApartments/AddressResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookify.Application.Apartments.SearchApartments;
+
+public sealed class AddressResponse
+{
+    public string Country { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+}
---
+++ Src/Bookify.Application/Apartments/SearchApartments/ApartmentResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Application.Apartments.SearchApartments;
+
+public sealed class ApartmentResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Currency { get; set; }
+    public AddressResponse Address { get; set; }
+}
