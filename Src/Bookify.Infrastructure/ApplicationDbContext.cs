b1f039c8016d24093801f5a8cbb319672717430d
rahimlotfi1368 <sinuhe_1368@yahoo.com>
2025-02-09 00:10:38 +0330

    start inrastructure layer

---
+++ Src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext(DbContextOptions options) 
+    : DbContext(options), IUnitOfWork;
