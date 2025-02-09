3ef81b7a5cde8562b8b0cf601adc97c42acca788
Rahim Lotfi Nemati <ra-lotfi@agri-bank.com>
2025-02-09 15:51:52 +0330

    Complete entity configurations

--- Src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ Src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -4,5 +4,16 @@ using Microsoft.Extensions.Options;
 
 namespace Bookify.Infrastructure;
 
-public sealed class ApplicationDbContext(DbContextOptions options) 
-    : DbContext(options), IUnitOfWork;
+public sealed class ApplicationDbContext(DbContextOptions options)
+    : DbContext(options), IUnitOfWork
+{
+    #region Overrides of DbContext
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(Application.DependencyInjection).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    #endregion
+}
